09d549cd1fe16099af65e3255914814bae3ef5c4
Willians Rodrigo <ivill.carvalho@gmail.com>
2026-03-04 21:10:52 -0300

    Add cover property to mangas

--- app/data/mangas.ts
+++ app/data/mangas.ts
@@ -2,9 +2,11 @@ export const mangas = [
   {
     id: "solo-leveling",
     title: "Solo Leveling",
+    cover: "/mangas/solo-leveling/cover.jpg",
     chapters: [
-      { number: 1, pages: 3 },
-      { number: 2, pages: 3 }
-    ]
-  }
+      { number: 1, pages: 4 },
+      { number: 2, pages: 4 },
+      { number: 3, pages: 4 },
+    ],
+  },
 ];
